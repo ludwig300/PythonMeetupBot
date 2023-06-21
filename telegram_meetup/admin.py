469d89f3891d544ae7083ff5a6774851731d4e36
ludwig300 <mr4silent@gmail.com>
2023-06-21 19:45:37 +0700

    Updated admin.py

--- telegram_meetup/admin.py
+++ telegram_meetup/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import User, Event, Report, Question
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'firstname', 'lastname', 'role')
+    list_filter = ('role',)
+    search_fields = ('username', 'firstname', 'lastname')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+    search_fields = ('title',)
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'speaker', 'datetime', 'event')
+    list_filter = ('speaker', 'event')
+    search_fields = ('title',)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'report', 'created_at')
+    list_filter = ('user', 'report')
+    search_fields = ('question',)
