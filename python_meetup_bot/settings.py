856daaff0b8c768fa2cc42a4713a09170124260e
ludwig300 <mr4silent@gmail.com>
2023-06-23 16:08:27 +0700

    Prepare for Heroku deployment

--- python_meetup_bot/settings.py
+++ python_meetup_bot/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # New setting
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
